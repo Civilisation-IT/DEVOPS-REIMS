8d77df2f1b1f3d11e2d007257742e1f97316aca3
Killian-Aidalinfo <killian.stein@aidalinfo.fr>
2026-02-24 16:23:20 +0100

    fix : correct method to get env

--- tp2/scripts/sample.js
+++ tp2/scripts/sample.js
@@ -3,17 +3,17 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '30s', target: process.env.VUS_30 ? parseInt(process.env.VUS_30) : 150 },
-    { duration: '1m30s', target: process.env.VUS_130 ? parseInt(process.env.VUS_130) : 350 },
+    { duration: '30s', target: __ENV.VUS_30 ? parseInt(__ENV.VUS_30, 10) : 150 },
+    { duration: '1m30s', target: __ENV.VUS_130 ? parseInt(__ENV.VUS_130, 10) : 350 },
     { duration: '20s', target: 0 },
   ],
 };
 
 export default function () {
-  if (!process.env.TARGET_URL) {
+  if (!__ENV.TARGET_URL) {
     throw new Error('TARGET_URL environment variable is not set');
   }
-  const res = http.get(process.env.TARGET_URL);
+  const res = http.get(__ENV.TARGET_URL);
   check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 }
